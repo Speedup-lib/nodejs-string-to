008d6a8f2c8d2d20e912ec499595fd2bea0c613c
Danial Manavi <dmanavi@live.com>
2020-09-04 11:16:51 +0430

    Remove duplicated nullables :D
    Changes to be committed:
    	modified:   src/parser/int.ts

--- src/parser/int.ts
+++ src/parser/int.ts
@@ -13,7 +13,7 @@ export class Parser implements IParser<number> {
      * @param val String value to parse
      * @param options radix (the second parameter in parseInt method)
      */
-    validateSync(val?: string, options?: validator.IsIntOptions | undefined): boolean {
+    validateSync(val?: string, options?: validator.IsIntOptions): boolean {
         // safely convert to string (for null and undefined values)
         return validator.isInt(val + '', options,);
     };
@@ -23,7 +23,7 @@ export class Parser implements IParser<number> {
      * @param val String value to parse
      * @param options radix (the second parameter in parseInt method)
      */
-    async validate(val?: string, options?: validator.IsIntOptions | undefined): Promise<boolean> {
+    async validate(val?: string, options?: validator.IsIntOptions): Promise<boolean> {
         return this.validateSync(val, options);
     }
 
@@ -33,7 +33,7 @@ export class Parser implements IParser<number> {
      * @param val String value to parse
      * @param options radix (the second parameter in parseInt method)
      */
-    parseSync(val?: string, options?: number | undefined): number {
+    parseSync(val?: string, options?: number): number {
         return parseInt(val!, options);
     }
 
@@ -42,7 +42,7 @@ export class Parser implements IParser<number> {
      * @param val String value to parse
      * @param options radix (the second parameter in parseInt method)
      */
-    async parse(val?: string, options?: number | undefined): Promise<number> {
+    async parse(val?: string, options?: number): Promise<number> {
         return this.parseSync(val, options);
     }
 
@@ -53,7 +53,7 @@ export class Parser implements IParser<number> {
      * @param defaultValue Default value if the provided string is not a valid number
      * @param options Contains both validator and parser options
      */
-    validateAndParseSync(val?: string, defaultValue?: number, options?: { validatorOptions?: validator.IsIntOptions | undefined, parserOptions?: number | undefined }): number | undefined {
+    validateAndParseSync(val?: string, defaultValue?: number, options?: { validatorOptions?: validator.IsIntOptions, parserOptions?: number }): number | undefined {
         return this.validateSync(val, options?.validatorOptions) ? this.parseSync(val, options?.parserOptions) : defaultValue;
     }
 
@@ -63,7 +63,7 @@ export class Parser implements IParser<number> {
      * @param defaultValue Default value if the provided string is not a valid number
      * @param options Contains both validator and parser options
      */
-    async validateAndParse(val?: string, defaultValue?: number, options?: { validatorOptions?: validator.IsIntOptions | undefined, parserOptions?: number | undefined }): Promise<number | undefined> {
+    async validateAndParse(val?: string, defaultValue?: number, options?: { validatorOptions?: validator.IsIntOptions, parserOptions?: number }): Promise<number | undefined> {
         return this.validateAndParseSync(val, defaultValue, options,);
     }
 };
