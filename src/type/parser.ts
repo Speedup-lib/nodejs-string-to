4cf682deafce795f3f2630f4b8c5b3831539bbb1
Danial Manavi <dmanavi@live.com>
2020-09-04 11:13:10 +0430

    Parser type update.
    Changes to be committed:
    	modified:   src/type/parser.ts

--- src/type/parser.ts
+++ src/type/parser.ts
@@ -2,18 +2,16 @@
  * Parser type
  */
 
-export type Validate = (val: string, options?: any) => Promise<boolean>
-export type ValidateSync = (val: string, options?: any) => boolean;
+export type ValidateSync = (val?: string, options?: any) => boolean;
+export type Validate = (val?: string, options?: any) => Promise<boolean>
 
-export type Parse<T> = (val: string, defaultValue?: T, options?: any) => Promise<T>;
-export type ParseSync<T> = (val: string, defaultValue?: T, options?: any) => T;
+export type ParseSync<T> = (val?: string, options?: any) => T;
+export type Parse<T> = (val?: string, options?: any) => Promise<T>;
+
+export type validateAndParseSync<T> = (val?: string, defaultValue?: T | undefined, options?: { validatorOptions?: any, parserOptions?: any }) => T | undefined;
+export type validateAndParse<T> = (val?: string, defaultValue?: T | undefined, options?: { validatorOptions?: any, parserOptions?: any }) => Promise<T | undefined>;
 
 export type ParserF<T> = {
-
-    /**
-     * Ensure that the value is parsable
-     */
-    validate: Validate,
 
     /**
      * Ensure that the value is parsable (Sync mode)
@@ -21,36 +19,64 @@ export type ParserF<T> = {
     validateSync: ValidateSync,
 
     /**
-     * Parse a property into type T
+     * Ensure that the value is parsable
      */
-    parse: Parse<T>,
+    validate: Validate,
+
 
     /**
      * Parse a property into type T (Sync mode)
      */
     parseSync: ParseSync<T>,
+
+    /**
+     * Parse a property into type T
+     */
+    parse: Parse<T>,
+
+
+    /**
+     * Validate and parse
+     */
+    validateAndParseSync: validateAndParseSync<T>;
+
+    /**
+     * Validate and parse
+     */
+    validateAndParse: validateAndParse<T>;
 };
 
 export interface Parser<T> {
-
-    /**
-     * Ensure that the value is parsable
-     */
-    validate: Validate;
 
     /**
      * Ensure that the value is parsable (Sync mode)
      */
     validateSync: ValidateSync;
 
+    /**
+     * Ensure that the value is parsable
+     */
+    validate: Validate;
+
+
+    /**
+     * Parse a property into type T (Sync mode)
+     */
+    parseSync: ParseSync<T>;
 
     /**
      * Parse a property into type T
      */
     parse: Parse<T>;
 
+
     /**
-     * Parse a property into type T (Sync mode)
+     * Validate and parse
      */
-    parseSync: ParseSync<T>;
+    validateAndParseSync: validateAndParseSync<T>;
+
+    /**
+     * Validate and parse
+     */
+    validateAndParse: validateAndParse<T>;
 };
