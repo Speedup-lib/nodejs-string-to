9ae2bcf71989b5c80a10bbbdbac7e9c2be16c4e7
Danial Manavi <dmanavi@live.com>
2020-09-06 13:07:33 +0430

    Add options to enable/disable a specific parser.
    Changes to be committed:
    	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
  * @speedup/string-to library
  */
 
-import { parser as boolParser } from './parser/bool';
-import { parser as dateParser } from './parser/date';
-import { parser as numberParser } from './parser/number';
+import { parser as boolParser, } from './parser/bool';
+import { parser as dateParser, } from './parser/date';
+import { parser as numberParser, } from './parser/number';
 
 export * as arrayParser from './parser/array';
 export * as boolParser from './parser/bool';
@@ -14,22 +14,58 @@ export * as intParser from './parser/int';
 export * as jsonParser from './parser/json';
 export * as numberParser from './parser/number';
 
+export type TryAllOptions = {
+
+    /**
+     * Enable number parser
+     */
+    number?: boolean,
+
+    /**
+     * Enable date parser
+     */
+    date?: boolean,
+
+    /**
+     * Enable bool parser
+     */
+    bool?: boolean,
+};
+
+export const defaultTryAllOptions: TryAllOptions = {
+    number: true,
+    date: true,
+    bool: true,
+};
+
 /**
  * Try converting the provided value into any primitive type
  * @param val Value to try parsing on
+ * @param options? Try all options
  */
-export const tryAnyToPrimitive = (val?: any): number | Date | boolean | string | null | undefined => {
+export const tryAnyToPrimitive = (val?: any, options?: TryAllOptions): number | Date | boolean | string | null | undefined => {
+
+    const mergedOptions: TryAllOptions = {
+        ...defaultTryAllOptions,
+        ...(options || {})
+    };
 
     if (val === undefined || val === null) { return val; }
 
-    const numberValue = numberParser.validateAndParseSync(val);
-    if (!!numberValue) { return numberValue; }
+    if (mergedOptions.number === true) {
+        const numberValue = numberParser.validateAndParseSync(val);
+        if (!!numberValue) { return numberValue; }
+    }
 
-    const dateValue = dateParser.validateAndParseSync(val);
-    if (!!dateValue) { return dateValue; }
+    if (mergedOptions.date === true) {
+        const dateValue = dateParser.validateAndParseSync(val);
+        if (!!dateValue) { return dateValue; }
+    }
 
-    const boolValue = boolParser.validateAndParseSync(val);
-    if (!!boolValue) { return boolValue; }
+    if (mergedOptions.bool === true) {
+        const boolValue = boolParser.validateAndParseSync(val);
+        if (!!boolValue) { return boolValue; }
+    }
 
     return val;
 };
@@ -37,15 +73,17 @@ export const tryAnyToPrimitive = (val?: any): number | Date | boolean | string |
 /**
  * Iterate over array items and parse all the values
  * @param arr Array to iterate over
+ * @param options? Try all options
  */
-export const parseArrayItems = async (arr: Array<any>): Promise<Array<any> | undefined> =>
-    parseArrayItemsSync(arr);
+export const parseArrayItems = async (arr: Array<any>, options?: TryAllOptions): Promise<Array<any> | undefined> =>
+    parseArrayItemsSync(arr, options,);
 
 /**
  * Iterate over array items and parse all the values
  * @param arr Array to iterate over
+ * @param options? Try all options
  */
-export const parseArrayItemsSync = (arr?: Array<any>): Array<any> | undefined => {
+export const parseArrayItemsSync = (arr?: Array<any>, options?: TryAllOptions): Array<any> | undefined => {
 
     if (arr === undefined || arr === null || Array.isArray(arr) === false) { return arr; }
 
@@ -53,15 +91,15 @@ export const parseArrayItemsSync = (arr?: Array<any>): Array<any> | undefined =>
 
         // recursive on array
         if (Array.isArray(itemValue)) {
-            return parseArrayItemsSync(itemValue);
+            return parseArrayItemsSync(itemValue, options,);
         }
         // recursive on object        
         else if (typeof itemValue === 'object') {
-            return parseObjectKeysSync(itemValue);
+            return parseObjectKeysSync(itemValue, options,);
         }
         // single-item to primitive type
         else {
-            return tryAnyToPrimitive(itemValue);
+            return tryAnyToPrimitive(itemValue, options,);
         }
     });
 };
@@ -71,14 +109,14 @@ export const parseArrayItemsSync = (arr?: Array<any>): Array<any> | undefined =>
  * Iterate over object keys and parse all the values
  * @param obj Object to iterate over
  */
-export const parseObjectKeys = async (obj?: { [key: string]: any }): Promise<{ [key: string]: any } | undefined> =>
-    parseObjectKeysSync(obj,);
+export const parseObjectKeys = async (obj?: { [key: string]: any }, options?: TryAllOptions): Promise<{ [key: string]: any } | undefined> =>
+    parseObjectKeysSync(obj, options,);
 
 /**
  * Iterate over object keys and parse all the values
  * @param obj Object to iterate over
  */
-export const parseObjectKeysSync = (obj?: { [key: string]: any }): { [key: string]: any } | undefined => {
+export const parseObjectKeysSync = (obj?: { [key: string]: any }, options?: TryAllOptions): { [key: string]: any } | undefined => {
 
     if (obj === null || obj === undefined) { return obj; }
 
@@ -91,15 +129,15 @@ export const parseObjectKeysSync = (obj?: { [key: string]: any }): { [key: strin
 
         // recursive on array
         if (Array.isArray(itemValue)) {
-            output[itemName] = parseArrayItemsSync(itemValue);
+            output[itemName] = parseArrayItemsSync(itemValue, options,);
         }
         // recursive on object        
         else if (typeof itemValue === 'object') {
-            output[itemName] = parseObjectKeysSync(itemValue);
+            output[itemName] = parseObjectKeysSync(itemValue, options,);
         }
         // single-item to primitive type
         else {
-            output[itemName] = tryAnyToPrimitive(itemValue);
+            output[itemName] = tryAnyToPrimitive(itemValue, options,);
         }
     });
 
